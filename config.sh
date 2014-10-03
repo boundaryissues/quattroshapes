b60b97ffbef6e782f78107da2bd3b57287947f80
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-03 10:26:43 -0400

    Rewrite/simplify import script.
    import.sh
    	-Rewrite the import script to download only a single shapefile
    	(currently `qs_neighborhoods`), as per conversation with
    	@randymeech.
    	-Add documentation comment.
    
    config.sh
    	-Redefine variables to reflect changes in `import.sh`.
    	-Add documentation comment.

--- config.sh
+++ config.sh
@@ -1,1 +1,6 @@
+# Configuration file for `import.sh`. Contains sensible defaults.
+
+field_to_split_on=name_adm1
 output_format=geojson
+dest_dir=polygons
+shapefile=qs_neighborhoods
--- import.sh
+++ import.sh
@@ -1,47 +1,35 @@
 #! /bin/bash
 
-download_dir=downloads
+#! /bin/bash
 
-download_shapefile(){
-	echo "Downloading shapefile $1."
-	local zip_name=$1.zip
-	cd $download_dir
-	wget --quiet http://static.quattroshapes.com/$zip_name
-	unzip $zip_name > /dev/null
-	rm $zip_name
-	cd ..
-}
-
-split_shapefile(){
-	echo "Processing shapefile $1."
-	mkdir $1
-	mapshaper $download_dir/$1.shp \
-		encoding=utf8 -split $2 -o format=$output_format $1 2> /dev/null
-}
-
-fix_localities(){
-	printf "\xff" |\
-		dd of=$download_dir/qs_localities.dbf \
-		bs=1 seek=144 count=1 conv=notrunc 2> /dev/null
-}
+# Description:
+#   Imports and partitions a Quattroshapes shapefile into GeoJSON extracts.
+#   Configuration variables are stored in `./config.sh`.
+#
+# Use:
+#   # update the configuration variables in config.sh, if you wish.
+#
+#   ./import.sh
 
 import(){
-	local poly_dir=quattroshapes
-	mkdir $poly_dir
-	cd $poly_dir
-	mkdir $download_dir
+	echo "Partionioning '$shapefile' in '$dest_dir/'."
 
-	for level in adm0 adm1 adm2 localadmin localities neighborhoods; do
-		download_shapefile qs_$level
-	done
+	echo "Downloading $shapefile.zip."
+	wget --quiet http://static.quattroshapes.com/$shapefile.zip
+	mkdir $dest_dir
+	mv $shapefile.zip $dest_dir
+	cd $dest_dir
 
-	fix_localities
+	echo "Unzipping $shapefile.zip."
+	unzip $shapefile.zip > /dev/null
 
-	for level in adm0 adm1 adm2 localadmin localities; do
-		local qs_name=qs_$level
-		split_shapefile $qs_name qs_adm0
-	done
-	split_shapefile qs_neighborhoods name_adm0
+	echo "Partionining/converting $shapefile.shp."
+	mapshaper $shapefile.shp encoding=utf8 \
+		-split $field_to_split_on -o format=$output_format . 2> /dev/null
+
+	echo "Cleaning up."
+	rm $shapefile.*
+	rm $shapefile.zip
 }
 
 source config.sh
