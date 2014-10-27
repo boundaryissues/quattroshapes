59a8127da105c0f900efb5cabfa79472f5d892ee
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-27 14:01:44 -0400

    Work on #1: partition by country first.
    import.sh
    	-Update the script to partition Quattroshapes by country, and
    	then partition each country-specific file by state.

--- import.sh
+++ import.sh
@@ -46,21 +46,46 @@ import(){
 	check_dep
 	get_shapefiles
 
+	# cd $dest_dir # TMP!
 	echo "Partionioning '$shapefile' in '$dest_dir/'."
 
 	echo "Partionining/converting $shapefile.shp."
 	mapshaper $shapefile.shp encoding=utf8 \
-		-split $field_to_split_on -o format=$output_format . 2> /dev/null
-
-	echo "Cleaning up."
-	rm $shapefile.*
+		-split gn_adm0_cc -o format=shapefile . 2> /dev/null
 
 	IFS=$'\n'
-	for file in $(find . -name "*.json"); do
-		local filename=${file%.json}
-		filename=${filename##./qs_neighborhoods-}
-		mv $file $filename.geojson
+	for country_file in $(find . -name "qs_neighborhoods-*.shp"); do
+		local country="${country_file%.shp}"
+		country="${country##./qs_neighborhoods-}"
+
+		local country_dir="$country"
+		if [ "$country_dir" = "" ]; then
+			country_dir="null"
+		fi
+
+		mkdir "$country_dir"
+
+		find . -name "qs_neighborhoods-$country.*" -type f \
+			| xargs -i mv "{}" "$country_dir"
+		cd "$country_dir"
+
+		mapshaper $country_file encoding=utf8 \
+			-split $field_to_split_on -o format=$output_format . 2> /dev/null
+
+		IFS=$'\n'
+		for sub_file in $(find . -name "*.json"); do
+			local filename=${sub_file%.json}
+			filename=${filename##./qs_neighborhoods-}
+			mv $sub_file $filename.geojson
+		done
+
+		find . -name "qs_neighborhoods-$country.*" -type f -print0 \
+			| xargs -0 rm
+		cd ..
 	done
+
+	echo "Cleaning up."
+	# rm $shapefile.*
 }
 
 source config.sh
